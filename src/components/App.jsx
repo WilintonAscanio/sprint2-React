f6eee2d4e55ddcb3dcf6bd198e9cb86b649f41af
WilintonAscanio <wilintonascanio12@hotmail.com>
2023-02-13 13:58:18 -0500

    Add structure

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './Header'
+import Pay from './Pay'
+import Services from './Services'
+
+const App = () => {
+    return (
+        <>
+            <Header />
+            <Pay />
+            <Services />
+        </>
+
+
+    )
+}
+
+export default App
