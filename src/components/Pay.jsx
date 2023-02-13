718f1e2930ec4d0f3b0b77e3ea6f654410f57cd4
WilintonAscanio <wilintonascanio12@hotmail.com>
2023-02-13 17:41:42 -0500

    Add main page

--- src/components/Pay.jsx
+++ src/components/Pay.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
+import blue from '../assets/Amex.svg'
+import paypal from '../assets/paypal.svg'
+import invex from '../assets/banco.jpg'
+import master from '../assets/Mastercard.svg'
+import visa from '../assets/Visa.svg'
+import oxxo from '../assets/oxxo.svg'
+import seven from '../assets/7-eleven.svg'
+import walmart from '../assets/walmart.svg'
+import ahorro from '../assets/farmacias.svg'
 
 const Pay = () => {
   return (
-    <div>Pay</div>
+
+    <>
+    <article className='payMethods'>
+      <h2>Pago Seguro</h2>
+      <section>
+        <div>
+          <p>Tarjeta de créedito, tarjeta de débito y pago electrónico</p>
+          <figure>
+            <img src={blue} alt="blue"/>
+            <img src={paypal} alt="paypal" />
+            <img src={invex} alt="invex" />
+            <img src={master} alt="master" />
+            <img src={visa} alt="visa" />
+          </figure>
+        </div>
+        <div>
+          <p>Efectivo en cualquiera de las sucursales participantes</p>
+          <figure>
+            <img src={oxxo} alt="oxxo" />
+            <img src= {seven} alt="7" />
+            <img src= {walmart} alt="walmart" />
+            <img src= {ahorro} alt="Ahorro" />
+          </figure>
+        </div>
+      </section>
+    </article>
+    </>
   )
 }
 
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,8 +1,63 @@
 import React from 'react'
+import car from '../assets/car.png'
+import vys from '../assets/vys.png'
+import team from '../assets/grupo.png'
+import bed from '../assets/dormir.png'
+import charge from '../assets/bateria.png'
 
 const Services = () => {
+
+  const services = [
+    {
+      id: 1,
+      icon : car,
+      title: 'Transporte',
+      description : 'Renta un auto o reserva un shuttle.'
+    },
+    {
+      id: 2,
+      icon : vys,
+      title: 'Vuelos + Hoteles',
+      description : 'Encuentra las mejores ofertas para tu viaje.'
+    },
+    {
+      id: 3,
+      icon : team,
+      title: 'Grupos',
+      description : 'Obtén una cotización para grupos de más de 9 personas.'
+    },
+    {
+      id: 4,
+      icon : bed,
+      title: 'Hoteles',
+      description : 'Reserva cualquier habitación en cualquier parte del mundo.'
+    },
+    {
+      id: 5,
+      icon : charge,
+      title: 'Carga',
+      description : 'Contamos con servicio de carga y mensajería.'
+    },
+  ]
   return (
-    <div>Services</div>
+    <>
+    <article className='services'>
+      <h2>Servicios disponibles</h2>
+      <section className='services__cards'>
+        {services.map((service, index) => (
+          <div key={index}>
+            <figure>
+              <img src={service.icon} alt={service.title} />
+            </figure>
+            <small>{service.title}</small>
+            <p>{service.description}</p>
+          </div>
+
+        ))}
+        
+      </section>
+    </article>
+    </>
   )
 }
 
