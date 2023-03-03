5edb92b55b236b272a3ef739e800f2799ed38c2a
WilintonAscanio <wilintonascanio12@hotmail.com>
2023-03-03 16:03:11 -0500

    Add seats function

--- src/components/flight/Seats.jsx
+++ src/components/flight/Seats.jsx
@@ -1,111 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Seat from './Seat';
 
 const Seats = () => {
+  const letras = ['A', 'B', 'C', 'D', 'E'];
+  const numeros = [1, 2, 3, 4, 5];
+  const [asientosSeleccionados, setAsientosSeleccionados] = useState([]);
+
+  const handleClickAsiento = (asiento) => {
+    if (asientosSeleccionados.includes(asiento)) {
+      setAsientosSeleccionados(asientosSeleccionados.filter(a => a !== asiento));
+    } else {
+      setAsientosSeleccionados([...asientosSeleccionados, asiento]);
+    }
+  }
+
   return (
-    <>
-    <section>
-          <small>A</small>
-          <small>B</small>
-          <small>C</small>
-          <strong></strong>
-          <small>D</small>
-          <small>E</small>
-          <small>F</small>
-      </section>
-      <p>Salida rápida</p>
-      <section className='seats__bottom'>
-        <small></small>
-        <small></small>
-        <small></small>
-        <strong>1</strong>
-        <small></small>
-        <small></small>
-        <small></small>
-      </section>
-      <section className='seats__bottom'>
-        <small></small>
-        <small></small>
-        <small></small>
-        <strong>2</strong>
-        <small></small>
-        <small></small>
-        <small></small>
-      </section>
-      <section className='seats__bottom'>
-        <small></small>
-        <small></small>
-        <small></small>
-        <strong>3</strong>
-        <small></small>
-        <small></small>
-        <small></small>
-      </section>
-      <section className='seats__bottom'>
-        <small></small>
-        <small></small>
-        <small></small>
-        <strong>4</strong>
-        <small></small>
-        <small></small>
-        <small></small>
-      </section>
-      <section className='seats__bottom'>
-        <small></small>
-        <small></small>
-        <small></small>
-        <strong>5</strong>
-        <small></small>
-        <small></small>
-        <small></small>
-      </section>
-      <p>Estandar</p>
-      <section className='seats__bottom'>
-        <small></small>
-        <small></small>
-        <small></small>
-        <strong>6</strong>
-        <small></small>
-        <small></small>
-        <small></small>
-      </section>
-      <section className='seats__bottom'>
-        <small></small>
-        <small></small>
-        <small></small>
-        <strong>7</strong>
-        <small></small>
-        <small></small>
-        <small></small>
-      </section>
-      <section className='seats__bottom'>
-        <small></small>
-        <small></small>
-        <small></small>
-        <strong>8</strong>
-        <small></small>
-        <small></small>
-        <small></small>
-      </section>
-      <section className='seats__bottom'>
-        <small></small>
-        <small></small>
-        <small></small>
-        <strong>9</strong>
-        <small></small>
-        <small></small>
-        <small></small>
-      </section>
-      <section className='seats__bottom'>
-        <small></small>
-        <small></small>
-        <small></small>
-        <strong>10</strong>
-        <small></small>
-        <small></small>
-        <small></small>
-      </section>
-    </>
-  )
+    <div>
+      {letras.map(letra => (
+        <div key={letra} className='seat'>
+          {numeros.map(numero => (
+            <Seat
+              key={letra + numero}
+              letra={letra}
+              numero={numero}
+              seleccionado={asientosSeleccionados.includes(letra + numero)}
+              onClick={handleClickAsiento}
+            />
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 }
+
 
 export default Seats
